00f5c89a1d1d0adbe6744f73222db379705c61dc
Te Vallee <astronomemusic@gmail.com>
2014-11-22 11:53:47 -1000

    initial live reload sass setup

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -2,18 +2,18 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function () {
-  return gulp.src('./sass/*scss')
-    .pipe(sass())
-    .pipe(gulp.des('./css'));
+  return gulp.src('./sass/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch_styles', function () {
   gulp.watch('./sass/*.scss', ['styles']);
-  gulp.watch('*./html', notifyLiveReload);
-  gulp.watch('./css/*.css', notifyLiveReload);
-});
+  gulp.watch('*.html', notifyLiveReload);
+  gulp.watch('css/*.css', notifyLiveReload);
+})
 
-gulp.task('express', function () {
+gulp.task('express', function() {
   var express = require('express');
   var app = express();
   app.use(require('connect-livereload')({port: 35729}));
@@ -37,4 +37,4 @@ gulp.task('livereload', function() {
   tinylr.listen(35729);
 });
 
-gulp.task('default', ['watch_styles', 'express', 'livereload']);
+gulp.task('default', ['watch_styles','express','livereload']);
