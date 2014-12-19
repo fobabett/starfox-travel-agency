12f638c2fd0a3c968de7d548caceed2efa29588a
Te Vallee <astronomemusic@gmail.com>
2014-12-18 19:17:02 -1000

    ejs

--- app/server.js
+++ app/server.js
@@ -3,9 +3,10 @@ var express = require('express');
 var app = express();
 // MIDDLEWARE
 app.use(express.static('public'));
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(rew, res) {
-  res.render('./views');
+  res.render('./index.ejs');
 });
 // EXPORT THIS FILE AS A MODULE
 module.exports.app = app;
